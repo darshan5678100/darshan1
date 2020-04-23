a060c8fd4e9226360220d92ab39ca4ce3fdafaf5
darshan5678100 <64181790+darshan5678100@users.noreply.github.com>
2020-04-23 19:47:10 +0530

    Add files via upload

---
+++ LibraryException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagmentService.exception
+{
+   public class LibraryException : Exception
+    {
+        public LibraryException() : base()
+            {
+            Console.WriteLine("OOppss something wents wrong!!");
+            }
+        public LibraryException(string Message) : base(Message)
+        {
+            Console.WriteLine("OOppss something wents wrong!!");
+        }
+        public LibraryException(string Message,Exception InnerException ) 
+        {
+
+        }
+       
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
