235222b2b6ed2063d7a24a9d233b6f9c3c9e3c3f
darshan5678100 <64181790+darshan5678100@users.noreply.github.com>
2020-04-24 14:08:45 +0530

    Add files via upload

---
+++ Login.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagmentService.dml
+{
+   public class Login
+    {
+        public static string AdminUsername="admin";
+        public static string AdminPassword="admin";
+        public string Username;
+        public string Password;
+        public int BookId1;
+        public int BookId2;
+        public int BookId3;
+        public Login(string name,string password)
+        {
+            Username = name;
+            Password = password;
+            
+        }
+    }
+}
