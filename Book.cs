d717091373ea44d2eee51c8a2f3f5fceae02266c
darshan5678100 <64181790+darshan5678100@users.noreply.github.com>
2020-04-24 14:08:23 +0530

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagmentService.dml
+{
+ public class Book
+    {
+        public int BookId;
+        public string BookName;
+        public int Copies;
+        public string Status;
+        public DateTime Date;
+        public string Author;
+
+        public Book(int id,string name,int copies,string status,DateTime date,string author)
+        {
+            BookId = id;
+            BookName = name;
+            Copies = copies;
+            Status = status;
+            Date = date;
+            Author = author;
+        }
+    }
+}
